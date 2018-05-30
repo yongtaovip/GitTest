217af6a40b2c07e7693b4be6a6857147a64f9146
yongtaovip <yongtao_vip@163.com>
2018-05-30 10:48:02 +0800

    demo first upload

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by 罗永涛 on 2018/5/30.
+//  Copyright © 2018年 罗永涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by 罗永涛 on 2018/5/30.
+//  Copyright © 2018年 罗永涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
